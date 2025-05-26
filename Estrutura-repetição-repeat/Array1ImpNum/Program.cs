cb20405b84929eef07d53d6b2f782a20a02fb623
Matheus <164927300+mathersweeknd@users.noreply.github.com>
2025-05-26 12:35:39 -0300

    Add files via upload

---
+++ Estrutura-repetição-repeat/Array1ImpNum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Array1{
+
+public static void Main(){
+
+// Declarando array inteiro com 7 números
+
+int[] Array1 = {23, 57, 19, 20, 21, 19, 8};
+
+// Loop para contar todos os números
+
+for (int i = 0; i < Array1.Length; i++){
+
+// Imprimindo o array no console
+
+Console.WriteLine("Número do Array: " +Array1[i]);
+
+}
+}
+}
